7d1df30aa9f06515fee04ae306484ad682c4f463
Kevin Bridges <kevin@wiiBridges.com>
2019-11-04 07:18:26 -0600

    pull in LATEST CRA serviceWorker.js (PWA may still be applicable)

--- src/features/common/pwa/serviceWorker.js
+++ src/features/common/pwa/serviceWorker.js
@@ -8,16 +8,16 @@
 // resources are updated in the background.
 
 // To learn more about the benefits of this model and instructions on how to
-// opt-in, read http://bit.ly/CRA-PWA
+// opt-in, read https://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
 );
 
 export function register(config) {
@@ -43,7 +43,7 @@ export function register(config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit http://bit.ly/CRA-PWA'
+            'worker. To learn more, visit https://bit.ly/CRA-PWA'
           );
         });
       } else {
@@ -56,46 +56,46 @@ export function register(config) {
 
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
-    .register(swUrl)
-    .then(registration => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
-        if (installingWorker == null) {
-          return;
-        }
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
-            if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See http://bit.ly/CRA-PWA.'
-              );
+           .register(swUrl)
+           .then(registration => {
+             registration.onupdatefound = () => {
+               const installingWorker = registration.installing;
+               if (installingWorker == null) {
+                 return;
+               }
+               installingWorker.onstatechange = () => {
+                 if (installingWorker.state === 'installed') {
+                   if (navigator.serviceWorker.controller) {
+                     // At this point, the updated precached content has been fetched,
+                     // but the previous service worker will still serve the older
+                     // content until all client tabs are closed.
+                     console.log(
+                       'New content is available and will be used when all ' +
+                       'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                     );
 
-              // Execute callback
-              if (config && config.onUpdate) {
-                config.onUpdate(registration);
-              }
-            } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+                     // Execute callback
+                     if (config && config.onUpdate) {
+                       config.onUpdate(registration);
+                     }
+                   } else {
+                     // At this point, everything has been precached.
+                     // It's the perfect time to display a
+                     // "Content is cached for offline use." message.
+                     console.log('Content is cached for offline use.');
 
-              // Execute callback
-              if (config && config.onSuccess) {
-                config.onSuccess(registration);
-              }
-            }
-          }
-        };
-      };
-    })
-    .catch(error => {
-      console.error('Error during service worker registration:', error);
-    });
+                     // Execute callback
+                     if (config && config.onSuccess) {
+                       config.onSuccess(registration);
+                     }
+                   }
+                 }
+               };
+             };
+           })
+           .catch(error => {
+             console.error('Error during service worker registration:', error);
+           });
 }
 
 function checkValidServiceWorker(swUrl, config) {
