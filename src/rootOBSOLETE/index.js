198a4fabed600739f36e00ea778f1d89c6a84643
Kevin Bridges <kevin@wiiBridges.com>
2019-11-04 07:08:45 -0600

    clear original konva sandbox code

--- src/rootOBSOLETE/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'index.css'; // ?? obsolete once we have material-ui in place
-import App from 'App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
