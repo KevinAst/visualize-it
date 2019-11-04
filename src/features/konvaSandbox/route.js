90bb8203c21da5c4e79da24a682bdc100fb7f0e7
Kevin Bridges <kevin@wiiBridges.com>
2019-11-04 09:47:57 -0600

    cleanup unused imports

--- src/features/konvaSandbox/route.js
+++ src/features/konvaSandbox/route.js
@@ -1,6 +1,5 @@
 import React               from 'react';
-import {featureRoute, 
-        PRIORITY}          from 'feature-router';
+import {featureRoute}      from 'feature-router';
 import KonvaSandboxScreen  from './KonvaSandboxScreen';
 
 // ***
@@ -10,7 +9,6 @@ import KonvaSandboxScreen  from './KonvaSandboxScreen';
 export default [
 
   featureRoute({
-    //priority: PRIORITY.HIGH,
     content({fassets, appState}) {
       // TODO: for now simply unconditionally display our Sandbox screen
       return <KonvaSandboxScreen/>;
