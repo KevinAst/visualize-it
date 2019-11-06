62fb4318634f5c5e58ed2a28b86ec585f86297b4
Kevin Bridges <kevin@wiiBridges.com>
2019-11-06 11:47:17 -0600

    Updates

---
+++ app/precache-manifest.6e06203a1377c1bbec726286ff814c4d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d051802acec9742f55ea4914e99db1c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fa3ba06671ca9ed3be4",
+    "url": "./static/js/2.a779056b.chunk.js"
+  },
+  {
+    "revision": "26e117ed1ee3977f7161",
+    "url": "./static/js/main.c10292b8.chunk.js"
+  },
+  {
+    "revision": "863f6bae2dde882d183e",
+    "url": "./static/js/runtime-main.2a4fd9ed.js"
+  }
+]);
