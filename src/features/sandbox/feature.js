c293e4555a5289b0cb00420e8486a02024e245f3
Kevin Bridges <kevin@wiiBridges.com>
2020-02-12 15:29:17 -0600

    fix recursive import issue

--- src/features/sandbox/feature.js
+++ src/features/sandbox/feature.js
@@ -1,18 +1,8 @@
 import {createFeature}       from 'feature-u';
 import featureFlags          from 'featureFlags'
-//? pull in from file
-// import {leftNavManager}      from 'features';
-import konvaSandboxSmartPkg  from './konvaSandbox/konvaSandboxSmartPkg';
-   // ?? WEIRD: without prior import: ReferenceError: Cannot access 'SmartModel' before initialization
-   //           - src/core/SmartPkg.js                                  is extending SmartModel (line 97) (in this scenerio SmartModel is NOT YET defined)
-   //           - src/core/PkgManager.js                                is importing SmartPkg   (line 1)
-   //           - src/core/SmartModel.js                                is being expanded       (I THINK)
-   //           - src/features/common/baseUI/comp/LeftNavMenuPallet.js  is being expanded       (I THINK)
-   //           - src/features/common/baseUI/LeftNavManager.js          is being expanded       (I THINK)
-   //           - src/features/common/baseUI/feature.js                 is being expanded       (I THINK)
-   //           - src/features/index.js                                 is being expanded       (I THINK)
-   //           - src/app.js                                            is being expanded       (I THINK)
-   //           - src/index.js                                          is being expanded       (I THINK)
+//? needed to: pull package in from code
+//import {leftNavManager}      from 'features';
+import /*konvaSandboxSmartPkg  from*/ './konvaSandbox/konvaSandboxSmartPkg'; // ??$$ "HACK" STILL NEEDED: till we register things like Valve1 a different way
 
 // feature: sandbox
 //          sandbox to play with konva.js
@@ -21,7 +11,7 @@ export default createFeature({
 
   enabled: featureFlags.sandbox,
 
-  //? pull in from file
+  //? pull package in from code
   //? appInit({showStatus, fassets, getState, dispatch}) {
   //?   leftNavManager.addLeftNav(konvaSandboxSmartPkg);
   //? },
--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import 'util/ErrorExtensionPolyfill';
+import 'core/SmartPkg';      // see "Resolve Circular Dependency" below
 export {default} from 'app'; // redirect to app.js
+
+/*
+  ***********************************
+  *** Resolve Circular Dependency ***
+  ***********************************
+
+  Because SmartPkg extends SmartModel, and SmartModel uses PkgManager,
+  which in turn relies on SmartPkg ... 
+
+  >>> SmartPkg must be expanded FIRST
+      AI: By modeling PkgManger.loadPkg() elsewhere, we MAY get around
+          this circular dependency
+*/
+
