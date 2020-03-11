7815fde2c573aeba1f7384436324b7d0bd52207e
Kevin Bridges <kevin@wiiBridges.com>
2020-03-11 07:51:27 -0500

    minor cleanup

--- src/features/common/baseUI/comp/FileMenu.js
+++ src/features/common/baseUI/comp/FileMenu.js
@@ -3,22 +3,24 @@ import React,
         useCallback,
         useMemo}        from 'react';
 
+import SmartComp        from 'core/SmartComp';
+import {openPkg}        from 'core/pkgPersist';
+import pkgManager       from 'core/pkgManager';
+
+import {leftNavManager,
+        tabManager}     from 'features';
+
+import discloseError    from 'util/discloseError';
+import verify           from 'util/verify';
+import {toast}          from 'util/notify';
+
+import {useFassets}     from 'feature-u';
+import {useSelector}    from 'react-redux'
+
 import Button           from '@material-ui/core/Button';
 import Menu             from '@material-ui/core/Menu';
 import MenuItem         from '@material-ui/core/MenuItem';
 import Typography       from '@material-ui/core/Typography';
-
-import {openPkg}        from 'core/pkgPersist';
-import {leftNavManager,
-        tabManager}     from 'features';
-import discloseError    from 'util/discloseError';
-import {toast}          from 'util/notify';
-
-// ?? new
-import SmartComp        from 'core/SmartComp';
-import {useFassets}     from 'feature-u';
-import {useSelector}    from 'react-redux'
-import pkgManager       from 'core/pkgManager';
 
 /**
  * FileMenu: our FileMenu component.
@@ -31,9 +33,9 @@ export default function FileMenu() {
   const openFileMenu = useCallback((event) => setAnchorFileMenu(event.currentTarget), []);
   _closeFileMenu     = useCallback(()      => setAnchorFileMenu(null),                []);
 
-  // ?? new
-  const fassets     = useFassets();
-  const activeTabId = useSelector( (appState) => fassets.sel.getActiveTabId(appState), [fassets] );
+  const fassets      = useFassets();
+  const activeTabId  = useSelector( (appState) => fassets.sel.getActiveTabId(appState), [fassets] );
+  const isActiveTab  = activeTabId ? true : false;
 
   return (
     <div>
@@ -59,8 +61,8 @@ export default function FileMenu() {
             onClose={closeFileMenu}>
 
         <MenuItem onClick={handleOpenPkg}>Open ...</MenuItem>
-        <MenuItem onClick={() => handleSavePkg(activeTabId)}>Save</MenuItem>
-        <MenuItem onClick={handleSaveAsPkg}>Save As ...</MenuItem>
+        <MenuItem onClick={() => handleSavePkg(activeTabId)}   disabled={!isActiveTab}>Save</MenuItem>
+        <MenuItem onClick={() => handleSaveAsPkg(activeTabId)} disabled={!isActiveTab}>Save As ...</MenuItem>
 
       </Menu>
     </div>
@@ -79,7 +81,7 @@ let _closeFileMenu = null;
 
 
 /**
- * Open a visualize-it SmartPkg from our local file system
+ * Open (i.e. load) a SmartPkg selected from the user's local file system.
  */
 async function handleOpenPkg() {
   closeFileMenu();
@@ -103,25 +105,65 @@ async function handleOpenPkg() {
 }
 
 /**
- * Save the ??supplied smartPkg to it's originating PkgResourcePath.
+ * Save the supplied SmartPkg (identified from the supplied tab) to
+ * it's original pkgResourcePath.
+ *
+ * @param {string} activeTabId - the tab identifier from which we
+ * determine the SmartPkg to save.
+ *
+ * @throws {Error} an Error is thrown when the SmartPkg could not be
+ * identified (an unexpected condition).
  */
 async function handleSavePkg(activeTabId) {
   closeFileMenu();
 
-  if (!activeTabId) {
-    toast.warn({msg: 'Your active tab identifies which package to save ... please activate a tab.'});
-    return;
-  }
+  const pkg = resolvePkg(activeTabId);
 
+  // ?? do something
+  toast.warn({msg: 'Save coming soon!'});
+}
+
+/**
+ * Save the supplied SmartPkg (identified from the supplied tab) to a
+ * user selected originating PkgResourcePath.
+ *
+ * @param {string} activeTabId - the tab identifier from which we
+ * determine the SmartPkg to save.
+ *
+ * @throws {Error} an Error is thrown when the SmartPkg could not be
+ * identified (an unexpected condition).
+ */
+async function handleSaveAsPkg(activeTabId) {
+  closeFileMenu();
+
+  const pkg = resolvePkg(activeTabId);
+
+  // ?? do something
+  toast.warn({msg: 'Save As coming soon!'});
+}
+
+
+/**
+ * Resolve the SmartPkg identified from the supplied tab.
+ *
+ * @param {string} activeTabId - the tab identifier from which we
+ * determine the SmartPkg.
+ *
+ * @returns {SmartPkg} the package belonging to the supplied tab.
+ *
+ * @throws {Error} an Error is thrown when the SmartPkg could not be
+ * identified (an unexpected condition).
+ */
+function resolvePkg(activeTabId) {
   // locate the package that contains the resource in the active tab
   const tabController = tabManager.getTabController(activeTabId);
   const targetObj     = tabController.getTarget(); // can be: Scene/Collage or SmartComp (for classes)
   let   pkg           = targetObj.getPackage();
 
   // for components (when classes are registered as SmartPkg entries),
-  // there is no registered package (because the class is the contained item in the package)
+  // there is no registered package (because the class is the contained item within the package)
   // ... in this case:
-  //     - we use the packate the component was created from (which is in fact the contained SmartPkg)
+  //     - we use the package the component was created from (which is in fact the contained SmartPkg)
   //     - ultimately, however, this pkg is not persistable (because it is based on class)
   if (!pkg && targetObj instanceof SmartComp) {
     const classRef     = targetObj.getClassRef();
@@ -129,15 +171,10 @@ async function handleSavePkg(activeTabId) {
     pkg = pkgManager.getPackage(smartPkgName);
   }
 
-  console.log(`?? now we can save the pkg: `, pkg);
-  toast.warn({msg: 'Save coming soon! ... see the logs for the pkg that will be saved'});
-}
+  // verify the package is resolved
+  verify(pkg, `***ERROR*** <FileMenu> "save/saveAs" operation ... could not locate the SmartPkg for the '${activeTabId}' active tab :-(`);
 
-/**
- * Save the ??supplied smartPkg to a user selected originating PkgResourcePath.
- */
-async function handleSaveAsPkg() {
-  closeFileMenu();
-
-  toast.warn({msg: 'Save coming soon!'});
+  // that's all folks :-)
+  // console.log(`xx SmartPkg resolved: `, pkg);
+  return pkg;
 }
