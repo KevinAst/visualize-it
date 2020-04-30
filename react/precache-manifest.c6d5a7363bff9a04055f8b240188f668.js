2fdfdd2e37b629049f9f9529a17318b550bb695e
Kevin Bridges <kevin@wiiBridges.com>
2020-04-30 06:54:14 -0500

    Updates

---
+++ react/precache-manifest.c6d5a7363bff9a04055f8b240188f668.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c92a59d78171e250b6d7ea779cd14bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8bd46de0977e9dbd742a",
+    "url": "./static/js/2.a96b3134.chunk.js"
+  },
+  {
+    "revision": "02638f15791544e57651",
+    "url": "./static/js/main.63b65ff2.chunk.js"
+  },
+  {
+    "revision": "863f6bae2dde882d183e",
+    "url": "./static/js/runtime-main.2a4fd9ed.js"
+  }
+]);
