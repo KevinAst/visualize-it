427f2d75043b7d760fba2c1f20dc45a4a21f9de5
Kevin Bridges <kevin@wiiBridges.com>
2020-05-04 13:21:20 -0500

    implement TreeView animation WITH full state retention

--- src/util/comp/TreeView.svelte
+++ src/util/comp/TreeView.svelte
@@ -1,42 +1,57 @@
+<script context="module">
+ // retain ModuleScoped expansion state for each tree node
+ // ... so as to retain grandchildren state when grandparents are collapsed :-)
+ const _expansionState = {
+   // accumTreeId: expanded <boolean>
+ };
+</script>
+
 <script>
  // component params
  export let tree;
- export let depth    = 0;
- export let omitRoot = false;
+ export let omitRoot    = false;
+ export let depth       = 0;     // INTERNAL: maintain depth
+ export let accumTreeId = '';    // INTERNAL: accumulative ID throughout tree
 
- $: top = depth === 0;
-
+ // provide a visually animated transition for tree node expansion/contraction
+ import {slide as myTrans} from 'svelte/transition';
+ 
  // decompose tree node
- // TODO: parameterize tree structure interpretation (via callback param)
- //       ... allowing the operation on ANY structure :-)
- //       ... wait for SmartPkg production usage
- //           I suspect may need to be a proprietary component for this task (with all the DnD and double-click, etc.)
+ // AI: parameterize tree structure interpretation (via callback param)
+ //     ... allowing the operation on ANY structure :-)
+ //     ... wait for SmartPkg production usage
+ //         I suspect may need to be a proprietary component for this task (with all the DnD and double-click, etc.)
  const {label, children} = tree;
 
+ // maintain the accumulative ID throughout our tree
+ accumTreeId += (accumTreeId ? ' - ' : '') + label;
+
  // maintain expansion state
- let   expanded        = false;
- $:    collapsed       = !expanded;
+ let   expanded        = _expansionState[accumTreeId] || false; // ... default to false on first occurance
  $:    arrowDown       = expanded;
- const toggleExpansion = () => expanded = !expanded;
+ const toggleExpansion = () => expanded = _expansionState[accumTreeId] = !expanded;
+
+ // maintain indicator as to whether this is the top-level
+ $: top = depth === 0;
 </script>
 
-{#if omitRoot && depth === 0 && children}
+{#if omitRoot && top && children}
   {#each children as child}
-    <svelte:self tree={child} depth={depth + 1} />
+    <svelte:self tree={child} depth={depth + 1} accumTreeId={accumTreeId}/>
   {/each}
 {:else}
-  <ul class:top>
+  <ul class:top transition:myTrans="{{duration:500}}">
     <li>
       {#if children}
         <span on:click={toggleExpansion}>
   		    <span class="arrow" class:arrowDown>&#x25b6</span>
   		  	{label}
   		  </span>
-        <div class:expanded class:collapsed>
+        {#if expanded}
           {#each children as child}
-            <svelte:self tree={child} depth={depth + 1} />
+            <svelte:self tree={child} depth={depth + 1} accumTreeId={accumTreeId}/>
           {/each}
-        </div>
+        {/if}
       {:else}
         <span>
           <span class="no-arrow-spacer"/>
@@ -62,19 +77,13 @@
    padding-left:    1.0rem;
  }
  .arrow {
-/* color:               red; */
+/* color:               red; /* AI: how to access scss vars */
    cursor:              pointer;
 	 display:             inline-block;
-	 transition-duration: 0.5s;
+   transition-duration: 500ms;
 	 transition-property: transform;
  }
  .arrowDown {
    transform: rotate(90deg);
- }
- .expanded {
-   display: block;
- }
- .collapsed {
-   display: none;
  }
 </style>
