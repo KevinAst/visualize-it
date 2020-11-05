c526e7be40dfa0d6143922ee8aff9ad32e2c7118
Kevin Bridges <kevin@wiiBridges.com>
2020-11-05 11:13:34 -0600

    optimize rapid fire dragenter/dragover

--- src/core/RenderSmartView.svelte
+++ src/core/RenderSmartView.svelte
@@ -24,24 +24,28 @@
    return () => view.unmount();
  });
 
- // allow drops (of DnD) based on polymorphic SmartObj.pastable()
- //  - ?? GRRR: must implement BOTH dragenter/dragover BECAUSE have to override the default implementation (which prevents a drop)
- // ?? optimize this for enter/over (over required but use cached results of enter) 
- function allowDrops(e) {
-   // console.log(`xx dragEnter on: `, {pallet: view.pallet, types: e.dataTransfer.types});
-
-   const isAllowed = view.pallet.pastable(e);
-
+ // allow DnD drops based on polymorphic SmartObj.pastable()
+ // ... GRRR: Must implement BOTH dragenter/dragover BECAUSE have to override the default implementation (which prevents a drop).
+ //           This is optimized by caching, since nothing changes (for us) in drag over.
+ let isAllowed = null;
+ function allowDrops_enter(e) {
+   // console.log(`xx allowDrops_enter on: `, {pallet: view.pallet, types: e.dataTransfer.types});
+   isAllowed = view.pallet.pastable(e);
+   allowDrops_over(e);
+ };
+ function allowDrops_over(e) {
+   // console.log(`xx allowDrops_over on: `);
    if (isAllowed) {
      e.preventDefault(); // allow drop (nullify default disallow behavior)
      // console.log(`xx dropEffect BEFORE: ${e.dataTransfer.dropEffect}`);
      e.dataTransfer.dropEffect = 'link'; // change cursor to reflect droppable
      // console.log(`xx dropEffect AFTER: ${e.dataTransfer.dropEffect}`);
    }
- };
+ }
 
- // perform drops (of DnD) based on polymorphic SmartObj.paste()
+ // perform DnD drop based on polymorphic SmartObj.paste()
  function handleDrop(e) {
+   // console.log(`xx handleDrop`);
    view.pallet.paste(e);
  };
 
@@ -49,7 +53,7 @@
 
 <div bind:this={divContainer}
      {...$$restProps}
-     on:dragenter={allowDrops}
-     on:dragover={allowDrops}
+     on:dragenter={allowDrops_enter}
+     on:dragover={allowDrops_over}
      on:drop|preventDefault={handleDrop}
      style="background-color: LightGray; width: {width}px; height: {height}px; border: 1px solid black;"/>
